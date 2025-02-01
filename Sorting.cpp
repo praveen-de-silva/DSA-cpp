852a9ca0e7455d4673cd295a3bf6101d463de9c1
Praveen De Silva <desilvabkp.23@uom.lk>
2025-02-01 09:20:41 +0530

    insertion sort

--- Sorting.cpp
+++ Sorting.cpp
@@ -10,34 +10,31 @@ void printArr(vector<int> arr) {
     cout << "\n";
 }
 
+void bubbleSort(vector<int> arr) {
+    
+}
+
+
 void insertionSort(vector<int> arr) {
-    int temp;
+    int temp, j;
     for (int i=1; i<arr.size(); i++) {
         temp = arr[i];
-        for (int j=(i-1); j>=0; j--) {
-            if (temp < arr[j]) {
-                arr[j+1] = arr[j];
-                printArr(arr);
-            } else {
-                arr[j+1] = temp;
-                printArr(arr);
-                break;
-            }
-            if (j==0) {
-                arr[j] = temp;
-                break;
-            }
+        j = i;
+        
+        while (j>0 && temp < arr[j-1]) {
+            arr[j] = arr[j-1];
+            j--;
         }
-        cout << "---" << endl;
+
+        arr[j] = temp;
+        printArr(arr);
     }
 }
 
 int main() {
     vector<int> myArr = {7,2,4,1,5,3};
 
-    insertionSort(myArr);
-
-    cout << "Try programiz.pro";
+    // insertionSort(myArr);
 
     return 0;
 }
