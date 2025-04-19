85507978e4780f932325ec7afae6e692900afc74
Praveen De Silva <desilvabkp.23@uom.lk>
2025-04-19 08:14:04 +0530

    Refactor Kruskal's algorithm implementation for clarity and consistency

--- .vscode/MST_KruskalsAlgorithm.cpp
+++ .vscode/MST_KruskalsAlgorithm.cpp
@@ -1,104 +1,84 @@
-// Kruskal's algorithm in C++
-
-#include <algorithm>
 #include <iostream>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 #define edge pair<int, int>
 
 class Graph {
 private:
-    vector<pair<int, edge> > G;  // graph
-    vector<pair<int, edge> > T;  // mst
-    int *parent;
-    int V;  // number of vertices/nodes in graph
+    vector<pair<int, edge>> graph;
+    vector<pair<int, edge>> mst;
+    int *parents;
+    int size;
     
 public:
-    Graph(int V) {
-        parent = new int[V];
-        //i 0 1 2 3 4 5
-        //parent[i] 0 1 2 3 4 5
+    Graph(int n) {
+        size = n;
+        parents = new int[n];
         
-        for (int i = 0; i < V; i++) {
-            parent[i] = i;    
+        for (int i=0; i<size; i++) {
+            parents[i] = i;
         }
         
-        G.clear();
-        T.clear();  
+        graph.clear();
+        mst.clear();
     }
     
-    void AddWeightedEdge(int u, int v, int w) {
-        G.push_back(make_pair(w, edge(u, v)));
+    void addEdge(int u, int v, int w) {
+        graph.push_back(make_pair(w, edge(u, v)));
     }
     
-    int find_set(int i) {
-        /*
-         * by get parent can detect cycles (i.e. if 2 nodes have same parent)
-         */
-        
-        if (i == parent[i]) {
-            return i;  // if i is the parent of itself   
+    int findSet(int u) {
+        if (u == parents[u]) {
+            return u;
         }
         
-        // Else if i is not the parent of itself
-        // Then i is not the representative of his set,
-        // so we recursively call Find on its parent
-        return find_set(parent[i]);
+        return findSet(parents[u]);
     }
     
-    void union_set(int u, int v) {
-        parent[u] = parent[v];
+    void unionSet(int u, int v) {
+        parents[u] = parents[v];
     }
     
     void kruskal() {
-        int i, uRep, vRep;
-        sort(G.begin(), G.end());  // increasing weight
+        int u, v;
+        sort(graph.begin(), graph.end());
         
-        for (i = 0; i < G.size(); i++) {
-            uRep = find_set(G[i].second.first);
-            vRep = find_set(G[i].second.second);
+        for (pair<int, edge> p : graph) {
+            u = findSet(p.second.first);
+            v = findSet(p.second.second);
             
-            if (uRep != vRep) {
-                T.push_back(G[i]);  // add to tree
-                union_set(uRep, vRep);
+            if (u != v) {
+                mst.push_back(p);
+                unionSet(u, v);
             }
-        }        
-    }
-    
-    void print() {
-        cout << "Edge :" << " Weight" << endl;
-        
-        for (int i = 0; i < T.size(); i++) {
-            cout << T[i].second.first << " - " << T[i].second.second << " : "
-                 << T[i].first;
-            cout << endl;
         }
     }
     
-    void printParents() {
-        for (int i=0; i<4; i++) {
-            cout << parent[i] << " ";
-        }  
-        cout << "\n";
+    void printGraph() {
+        for (pair<int, edge> p : mst) {
+            cout << p.second.first << "-" << p.second.second << " (" << p.first << ")" << endl;
+        }
+        cout << endl;
     }
+    
 };
 
-
-
 int main() {
-    Graph g(4);
-  
-    g.AddWeightedEdge(0, 1, 20);
-    g.AddWeightedEdge(0, 2, 200);
-    g.AddWeightedEdge(0, 3, 50);
-    g.AddWeightedEdge(1, 2, 70);
-    g.AddWeightedEdge(1, 3, 100);
-    g.AddWeightedEdge(2, 3, 60);
-
-    g.kruskal();
-    g.print();
-    g.printParents();
     
+    Graph gp(4);
+    
+    gp.addEdge(0, 1, 20);
+    gp.addEdge(0, 2, 200);
+    gp.addEdge(0, 3, 50);
+    gp.addEdge(1, 2, 70);
+    gp.addEdge(1, 3, 100);
+    gp.addEdge(2, 3, 60);
+    
+    gp.kruskal();
+    gp.printGraph();
+
     return 0;
 }
